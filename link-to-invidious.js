9e874ac1548a9f6e5933adff5543542f37ee8f60
paulobtn <paulonetodev@gmail.com>
2021-10-23 11:30:51 -0300

    adding href to the link

--- link-to-invidious.js
+++ link-to-invidious.js
@@ -1,22 +1,23 @@
 // ==UserScript==
-// @name        link to watch on invidious
+// @name        link-to-invidious
 // @namespace   paulobtn's userscripts
 // @description add a link to indivious on youtube pages
 // @include     /^http(s|)://(www\.|)youtube\.com/.*$/
 // @include     /^http(s|)://youtu\.be/.*$/
+// @exclude      *://*.youtube.com/subscribe_embed?*
 // @version     1.0
 // ==/UserScript==
 
 //set your favorite Invidious instance
-var instance='vid.puffyan.us'; 
+var instance='vid.puffyan.us';
 
 //find the video name
 var title=Array.prototype.slice.call(document.getElementsByTagName('h1'));
 
-//observe changes and add button if it doesn't exist
+//observe changes during load and add the link to invidious after each one
 var observer=new MutationObserver(function(mutations){
   mutations.forEach(function(mutation){
-      var skip=Array.prototype.slice.call(mutation.target.getElementsByClassName('skipinv'));
+      var skip=Array.prototype.slice.call(mutation.target.getElementsByClassName('inv-btn'));
       if(skip<1){
           title=Array.prototype.slice.call(mutation.target.getElementsByTagName('h1'));
           addbtn();
@@ -24,10 +25,10 @@ var observer=new MutationObserver(function(mutations){
   });
 });
 
-// add link to invidious
 addbtn();
 observer.observe(document.body,{childList:true,subtree:true});
 
+// add link to invidious
 function addbtn(){
     for(var i=0;i<title.length;i++){
         var btn=document.createElement('a');
@@ -37,15 +38,10 @@ function addbtn(){
                                   display:inline-block;font-weight:400;
                                   padding:.3rem;"
                        >Watch on ${instance}</h2>`;
-        btn.href='javascript:void(0)';
-        btn.onclick=function(){redir();};
-        btn.className='skipinv';
+        btn.className='inv-btn';
+        var url=new URL(window.location.href);
+        url.hostname=instance;
+        btn.href=url;
         title[i].parentNode.appendChild(btn);
     }
-}
-
-function redir(){
-    var url=new URL(window.location.href);        
-    url.hostname=instance;
-    location.href=url;
 }
